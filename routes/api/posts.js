c0cb0ff29d296eef13ef91abbbc52db70f080e98
Bishop Bader <bflatbader@users.noreply.github.com>
2019-12-21 16:59:06 -0500

    added routes to like/unlike posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -83,6 +83,64 @@ router.post(
 	}
 );
 
+// PUT ROUTES
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Verify post does not belong to logged in user
+		if (post.user.toString() === req.user.id) {
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+        // Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+
+        // Add user to likes
+        post.likes.unshift({ user: req.user.id });
+
+        // Save post
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Like a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        }
+
+        // Get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+
+        // Save post
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 // DELETE ROUTES
 
 // @route   DELETE api/posts/:id
