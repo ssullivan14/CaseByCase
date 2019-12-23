b19c39b8df318c3c34a54c002fa0a057db833caf
Bishop Bader <bflatbader@users.noreply.github.com>
2019-12-22 20:15:53 -0500

    logout now functions

--- client/src/components/Layout/Navbar/Navbar.js
+++ client/src/components/Layout/Navbar/Navbar.js
@@ -1,24 +1,78 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../../images/logo.png'
+import React, { Fragment } from 'react';
+import './Navbar.css';
+import logo from '../../../images/logo.png';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../../actions/auth';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark">
-            <a className="navbar-brand" id="brand" href="/"><img src={logo} className="img-fluid smLogo" alt="Case By Case logo" /></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup"></div>
-                <div className="navbar-nav">
-                <a className="nav-item nav-link" href="/about">About&nbsp;</a>
-                <a className="nav-item nav-link" href="/contact"><i className="far fa-paper-plane menuIcons"></i>&nbsp;Contact Us</a>
-                <Link to='/register' className="nav-item nav-link"><i className="fas fa-user-plus menuIcons"></i>&nbsp;Register</Link>
-                <Link to='/login' className="nav-item nav-link" href="/contact"><i className="fas fa-sign-in-alt menuIcons"></i>&nbsp;Login</Link>
-            </div>
-        </nav>
-    )
-}
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+	const authLinks = (
+		<div className='navbar-nav'>
+			<a onClick={logout} href='/' className='nav-item nav-link'>
+				<i className='fas fa-sign-out-alt menuIcons'></i>&nbsp;Logout
+			</a>
+		</div>
+	);
 
-export default Navbar
+	const guestLinks = (
+		<div className='navbar-nav'>
+			<Link to="/about" className='nav-item nav-link'>
+				About&nbsp;
+			</Link>
+			<Link to='/contact' className='nav-item nav-link'>
+				<i className='far fa-paper-plane menuIcons'></i>&nbsp;Contact Us
+			</Link>
+			<Link to='/register' className='nav-item nav-link'>
+				<i className='fas fa-user-plus menuIcons'></i>&nbsp;Register
+			</Link>
+			<Link to='/login' className='nav-item nav-link'>
+				<i className='fas fa-sign-in-alt menuIcons'></i>&nbsp;Login
+			</Link>
+		</div>
+	);
+
+	return (
+		<nav className='navbar navbar-expand-lg navbar-dark'>
+			<a className='navbar-brand' id='brand' href='/'>
+				<a className='navbar-brand' id='brand' href='/'>
+					<img
+						src={logo}
+						className='img-fluid smLogo'
+						alt='Case By Case logo'
+					/>
+				</a>
+			</a>
+			<button
+				className='navbar-toggler'
+				type='button'
+				data-toggle='collapse'
+				data-target='#navbarNavAltMarkup'
+				aria-controls='navbarNavAltMarkup'
+				aria-expanded='false'
+				aria-label='Toggle navigation'
+			>
+				<span className='navbar-toggler-icon'></span>
+			</button>
+			<div
+				className='collapse navbar-collapse justify-content-end'
+				id='navbarNavAltMarkup'
+			></div>
+			{/* Display links, determine appropriate based on whether user is logged in or not */}
+			{!loading && (
+				<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+			)}
+		</nav>
+	);
+};
+
+Navbar.propTypes = {
+	logout: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
