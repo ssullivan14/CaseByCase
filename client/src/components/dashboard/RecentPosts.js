f953dcf334b7b2bb97f5f021043bde2f4c7621a1
Bishop Bader <bflatbader@users.noreply.github.com>
2020-01-06 10:38:23 -0500

    creating recent posts component

---
+++ client/src/components/dashboard/RecentPosts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RecentPosts = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+RecentPosts.propTypes = {
+
+}
+
+export default RecentPosts
