5c402054da82875fc8fd40480aef361baa12f4ee
Anthony Yonta <atischillin@gmail.com>
2020-01-21 20:46:10 -0500

    added dynamic searchtype between city / state

--- client/src/components/results/GMap.js
+++ client/src/components/results/GMap.js
@@ -1,79 +1,63 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { GoogleMap, withScriptjs,withGoogleMap, Marker} from 'react-google-maps';         
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from "react-google-maps";
 import { connect } from "react-redux";
 import mapStyles from "./MapStyles";
 
-    // VARIABLES
-    var locations;
-    var zoomOptions;
-    var centerOption = {};
-    // console.log(locations[0]);
+// VARIABLES
+var locations;
+var centerOption = {};
 
-    // const zoomSwitch = () => {
-    //   if (searchType === 'state') {
-    //     zoomOptions = 7
-    //   }
-    //   else {
-    //     zoomOptions = 10
-    //   }
-    // }
+const searchRequest = JSON.parse(localStorage.getItem("searchRequest"));
+var searchType = searchRequest.searchType
 
-const Map = ({ persons, loading }) => {    
+const Map = ({ persons, loading }) => {
   console.log(locations);
 
-    return (
- 
-        <GoogleMap
-          defaultZoom={10}
-          defaultCenter={locations[0]}
-          defaultOptions={{ styles: mapStyles }}
-          
-        >
-          {locations.map((locations) =>(
-                          <Marker 
-                          position={locations}
-                           />
-         ) )}
-    
-          </GoogleMap>
-    )
-}
+  return (
+    <GoogleMap
+      defaultZoom={searchType === "state" ? ( 7 ) : ( 10 )}
+      defaultCenter={locations[0]}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {locations.map(locations => (
+        <Marker position={locations} />
+      ))}
+    </GoogleMap>
+  );
+};
 const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function GMap({ persons, loading }) {
   locations = [];
-persons.forEach(person => {
-    let temp = {};                                
-    temp['lat'] = parseFloat(person.Latitude);
+  persons.forEach(person => {
+    let temp = {};
+    temp["lat"] = parseFloat(person.Latitude);
     temp["lng"] = parseFloat(person.Longitude);
     locations.push(temp);
     // console.log(temp);
-        
-});
-// return locations
+  });
+  // return locations
 
-// console.log(locations);
+  // console.log(locations);
 
-
-  return  (
+  return (
     <div style={{ width: "100%", height: "45vh" }}>
       <MapWrapped
-      
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_GOOGLE_KEY
-        }`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
-
     </div>
   );
-
 }
 
 Map.propTypes = {
-    person: PropTypes.object.isRequired
-  };
-  
+  person: PropTypes.object.isRequired
+};
