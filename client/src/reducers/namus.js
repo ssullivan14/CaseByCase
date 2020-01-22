5b339c1199a7ab30f05b9170c57ab3d9abe5a9be
digitalsamsquanch <digital.samsquanch@protonmail.com>
2020-01-21 20:12:35 -0500

    added api route, action, and reducer for namus case info

--- client/src/reducers/namus.js
+++ client/src/reducers/namus.js
@@ -1,30 +1,37 @@
-import { GET_NAMUS, NAMUS_ERROR } from '../actions/types';
+import { GET_NAMUS, NAMUS_ERROR } from "../actions/types";
 
 const initialState = {
-	persons: [],
-	loading: true,
-	error: {}
+  persons: [],
+  loading: true,
+  error: {}
 };
 
 export default function(state = initialState, action) {
-	const { type, payload } = action;
+  const { type, payload } = action;
 
-	switch (type) {
-		case GET_NAMUS:
-			return {
-				...state,
-				persons: payload,
-				loading: false
-			};
+  switch (type) {
+    case GET_NAMUS:
+      return {
+        ...state,
+        persons: payload,
+        loading: false
+      };
 
-		case NAMUS_ERROR:
-			return {
-				...state,
-				error: payload,
-				loading: false
-			};
+    case NAMUS_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
 
-		default:
-			return state;
-	}
+    case NAMUS_CASE:
+      return {
+        ...state,
+        namusCase: payload,
+        loading: false
+      };
+
+    default:
+      return state;
+  }
 }
