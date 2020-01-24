094e9b8535a7b6598de57243920e468d1c9ade3a
Bishop Bader <bflatbader@gmail.com>
2020-01-23 20:37:04 -0500

    corrected spelling mistakes

--- models/Unidentified.js
+++ models/Unidentified.js
@@ -1,104 +1,107 @@
 const mongoose = require('mongoose');
 const UnidentifiedSchema = new mongoose.Schema({
-    casenumber : {
-        type: String,
-      },
-      createdDateTime : {
-        type: Date,
-      },
-      modifiedDateTime : {
-        type: Date,
-      },
-      gender : {
-        type: String,
-      },
-      ethinicity : {
-        type : String,
-      },
-      age_group : {
-        type : String,
-      },
-      estimated_age_from : {
-        type : String,
-      },
-      estimated_age_to : {
-        type : String,
-      },
-      estimated_height_from : {
-        type : Number,
-      },
-      estimated_height_to : {
-        type : Number,
-      },
-      estimated_weight_from : {
-        type : Number,
-      },
-      estimated_weight_to : {
-        type : Number,
-      },
-      date_found : {
-        type : Date,
-      },
-      city : {
-        type : String,
-      },
-      address : {
-        type : String,
-      },
-      state : {
-        type : String,
-      },
-      zipcode : {
-        type : Number,
-      },
-      circumstanceOfRecovery : {
-        type : String,
-      },
-      latitude : {
-        type : String,
-      },
-      longitude : {
-        type : String,
-      },
-      conditionOfRemains : {
-        type : String,
-      },
-      clothinsAndAccessories : {
-        type : String,
-      },
-      clothingAndAccessories_location : {
-        type : String,
-      },
-      physicalDescription_hairColor : {
-        type : String,
-      },
-      headHairDescription : {
-        type : String,
-      },
-      facialHairDescription : {
-        type : String,
-      },
-      leftEyeColor : {
-        type : String,
-      },
-      rightEyeColor : {
-        type : String,
-      },
-      physicalFeatureDescriptions : {
-        type : String,
-      },
-      images : {
-        type : String,
-      },
-      investigatingAgencies_name : {
-        type : String,
-      },
-      investigatingAgencies_caseNumber : {
-        type : String,
-      },
-      imageThumbnail : {
-        type : String,
-      }
-    
+  casenumber: {
+    type: String,
+  },
+  createdDateTime: {
+    type: Date,
+  },
+  modifiedDateTime: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+  },
+  ethinicity: {
+    type: String,
+  },
+  age_group: {
+    type: String,
+  },
+  estimated_age_from: {
+    type: String,
+  },
+  estimated_age_to: {
+    type: String,
+  },
+  estimated_height_from: {
+    type: Number,
+  },
+  estimated_height_to: {
+    type: Number,
+  },
+  estimated_weight_from: {
+    type: Number,
+  },
+  estimated_weight_to: {
+    type: Number,
+  },
+  date_found: {
+    type: Date,
+  },
+  city: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  zipcode: {
+    type: Number,
+  },
+  circumstancesOfRecovery: {
+    type: String,
+  },
+  latitude: {
+    type: String,
+  },
+  longitude: {
+    type: String,
+  },
+  conditionOfRemains: {
+    type: String,
+  },
+  clothingAndAccessories: {
+    type: String,
+  },
+  clothingAndAccessories_location: {
+    type: String,
+  },
+  physicalDescription_hairColor: {
+    type: String,
+  },
+  headHairDescription: {
+    type: String,
+  },
+  facialHairDescription: {
+    type: String,
+  },
+  leftEyeColor: {
+    type: String,
+  },
+  rightEyeColor: {
+    type: String,
+  },
+  physicalFeatureDescriptions: {
+    type: String,
+  },
+  images: {
+    type: String,
+  },
+  investigatingAgencies_name: {
+    type: String,
+  },
+  investigatingAgencies_caseNumber: {
+    type: String,
+  },
+  imageThumbnail: {
+    type: String,
+  },
+  imagePoster: {
+    type: String,
+  }
+
 });
 module.exports = Unidentified = mongoose.model('unidentified', UnidentifiedSchema);
--- routes/api/unidentified.js
+++ routes/api/unidentified.js
@@ -2,22 +2,18 @@
 // DEPENDENCIES
 const express = require('express');
 const router = express.Router();
-const unIdentified = require('../../models/Unidentified');
+const unid = require('../../models/Unidentified');
 
 
 //Routes(to hit route /api/unidentified/)
 router.get('/', async (req, res) => {
     //find results from namus collection in the db with a query that finds all in our db
     try {
-        const unidentified = await unIdentified.find();
-        //     {
-        //     city: req.body.city,
-        //     state: req.body.state,
-        //     modifiedDateTime: req.body.state
-        // });
+        const unidentified = await unid.find({});
+
         res.json(unidentified);
-    } 
-    
+    }
+
     catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
