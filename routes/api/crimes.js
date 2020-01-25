eadbc9c23d94b0f9345acf7aacdac08956796b21
digitalsamsquanch <digital.samsquanch@protonmail.com>
2020-01-25 11:00:11 -0500

    all crimes api route is live

--- routes/api/crimes.js
+++ routes/api/crimes.js
@@ -1,27 +1,41 @@
 //connect to database with Express and us postman to test
 // DEPENDENCIES
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const allCrimes = require('../../models/Crimes');
-
+const allCrimes = require("../../models/Crimes");
 
 //Routes(to hit route /api/cimes)
-router.get('/', async (req, res) => {
-    //find results from namus collection in the db with a query that finds all in our db
-    try {
-        const crimes = await allCrimes.find({
-            Offense: req.body.Offense,
-            Date: req.body.Date,
-            State: req.body.State,
-            City: req.body.City
+router.get("/", async (req, res) => {
+  //find results from namus collection in the db with a query that finds all in our db
+  try {
+    const crimes = await allCrimes.find({
+      Offense: req.body.Offense,
+      Date: req.body.Date,
+      State: req.body.State,
+      City: req.body.City
+    });
+    res.json(crimes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
+router.get("/case/:case_id", async (req, res) => {
+  try {
+    const crimeCase = await allCrimes.findOne({
+      _id: req.params.case_id
+    });
 
-        });
-        res.json(crimes)
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+    if (!crimeCase) return res.status(400).json({ msg: "Case not found" });
+    res.json(crimeCase);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Case not found" });
     }
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
