e4f318b4d795f37bc048d4d57e4c1a3cc1e3e954
Bishop Bader <bflatbader@gmail.com>
2020-01-25 15:57:58 -0500

    began work on unidentified component

--- client/src/components/results/Results.js
+++ client/src/components/results/Results.js
@@ -5,17 +5,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getNamus } from '../../actions/namus';
 import { getCrimes } from '../../actions/crimes';
+import { getUnidentified } from '../../actions/unidentified';
 import Spinner from '../Layout/Spinner/Spinner';
 import Pagination from './Pagination';
 import MissingPersonItem from './MissingPersonItem';
 import SocrataItem from './SocrataItem';
+import UnidentifiedPersonItem from './UnidentifiedPersonItem';
 import './Results.css';
 
 const Results = ({
 	getNamus,
 	namus: { persons, loading },
 	getCrimes,
-	crimes: { incidents, crimeLoading }
+	crimes: { incidents, crimeLoading },
+	getUnidentified,
+	unidentified: { unIDpersons, unIDloading }
 }) => {
 	// Get search request out of local storage and convert back to an object
 	const searchRequest = JSON.parse(localStorage.getItem('searchRequest'));
@@ -31,12 +35,12 @@ const Results = ({
 				getNamus(searchRequest);
 				break;
 			case 'unidentified persons':
-				console.log('Unidentified persons search');
+				getUnidentified(searchRequest);
 				break;
 			default:
 				getCrimes(searchRequest);
 		}
-	}, [getNamus, getCrimes]);
+	}, [getNamus, getCrimes, getUnidentified]);
 
 	const socrataHeader = (
 		<Fragment>
@@ -54,21 +58,22 @@ const Results = ({
 		<Fragment>
 			<th scope='col'>Case Number</th>
 			<th scope='col'>Date Found</th>
+			<th scope='col'>Gender</th>
+			<th scope='col'>Estimated Age</th>
 			<th scope='col'>Description</th>
-			<th scope='col'>Address</th>
-			<th scope='col'>Location Details</th>
+			<th scope='col'>Location</th>
 			<th scope='col'>Images</th>
 		</Fragment>
 	);
-	
-	function waitForResults(variable){
-		if(typeof variable !== "undefined"){
+
+	function waitForResults(variable) {
+		if (typeof variable !== "undefined") {
 			getCurrentResults = variable.slice(
 				indexofFirstResult,
 				indexOfLastResult
 			);
 		}
-		else{
+		else {
 			setTimeout(waitForResults, 1000);
 		}
 	}
@@ -82,7 +87,7 @@ const Results = ({
 			waitForResults(persons);
 			break;
 		case 'unidentified persons':
-			console.log('Unidentified persons search');
+			waitForResults(unIDpersons);
 			break;
 		case 'assault':
 		case 'battery':
@@ -98,67 +103,70 @@ const Results = ({
 	const paginate = pageNumber => setCurrentPage(pageNumber);
 
 	return (loading && persons === null) ||
-		(crimeLoading && incidents === null) ? (
-		<Spinner />
-	) : (
-		<Fragment>
-			<div className='row'>
-				<div className='col-md-12'>
-					<Link to='/search' className='btn btn-secondary back-btn'>
-						<i id='toggleIcon' className='fa fa-angle-double-down'></i> Back to
-						Search
+		(crimeLoading && incidents === null) || (unIDloading && unIDpersons === null) ? (
+			<Spinner />
+		) : (
+			<Fragment>
+				<div className='row'>
+					<div className='col-md-12'>
+						<Link to='/search' className='btn btn-secondary back-btn'>
+							<i id='toggleIcon' className='fa fa-angle-double-down'></i> Back to
+							Search
 					</Link>
+					</div>
 				</div>
-			</div>
-			<h1 className='page-header'>Search Results</h1>
-			<p className='lead'>
-				<i className='fas fa-clipboard-list gold-icon'></i>&nbsp;&nbsp;
+				<h1 className='page-header'>Search Results</h1>
+				<p className='lead'>
+					<i className='fas fa-clipboard-list gold-icon'></i>&nbsp;&nbsp;
 				{searchRequest.location} > {searchRequest.incidentType} >{' '}
-				<Moment format='MM/DD/YYYY'>{searchRequest.startDate}</Moment> -{' '}
-				<Moment format='MM/DD/YYYY'>{searchRequest.endDate}</Moment>
-			</p>
-			<table className='table table-dark table-striped'>
-				<thead>
-					<tr>
-						{/* Check incident type and display appropriate headers */}
+					<Moment format='MM/DD/YYYY'>{searchRequest.startDate}</Moment> -{' '}
+					<Moment format='MM/DD/YYYY'>{searchRequest.endDate}</Moment>
+				</p>
+				<table className='table table-dark table-striped'>
+					<thead>
+						<tr>
+							{/* Check incident type and display appropriate headers */}
+							{searchRequest.incidentType === 'unidentified persons' ? (
+								unidentifiedHeader
+							) : searchRequest.incidentType === 'missing person' ? (
+								<Fragment></Fragment>
+							) : (
+										socrataHeader
+									)}
+						</tr>
+					</thead>
+					<tbody>
 						{searchRequest.incidentType === 'unidentified persons' ? (
-							unidentifiedHeader
+							<UnidentifiedPersonItem persons={getCurrentResults} loading={unIDloading} />
 						) : searchRequest.incidentType === 'missing person' ? (
-							<Fragment></Fragment>
+							<MissingPersonItem persons={getCurrentResults} loading={loading} />
 						) : (
-							socrataHeader
-						)}
-					</tr>
-				</thead>
-				<tbody>
-					{searchRequest.incidentType === 'unidentified persons' ? (
-						unidentifiedHeader
-					) : searchRequest.incidentType === 'missing person' ? (
-						<MissingPersonItem persons={getCurrentResults} loading={loading} />
-					) : (
-						<SocrataItem incidents={getCurrentResults} loading={crimeLoading} />
-					)}
-				</tbody>
-			</table>
-			<Pagination
-				resultsPerPage={resultsPerPage}
-				totalResults={persons.length}
-				paginate={paginate}
-			/>
-		</Fragment>
-	);
+									<SocrataItem incidents={getCurrentResults} loading={crimeLoading} />
+								)}
+					</tbody>
+				</table>
+				<Pagination
+					resultsPerPage={resultsPerPage}
+					totalResults={persons.length}
+					paginate={paginate}
+				/>
+			</Fragment>
+		);
 };
 
 Results.propTypes = {
 	getNamus: PropTypes.func.isRequired,
 	namus: PropTypes.object.isRequired,
 	getCrimes: PropTypes.func.isRequired,
-	crimes: PropTypes.object.isRequired
+	crimes: PropTypes.object.isRequired,
+	getUnidentified: PropTypes.func.isRequired,
+	unidentified: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
 	namus: state.namus,
-	crimes: state.crimes
+	crimes: state.crimes,
+	unidentified: state.unidentified
 });
 
-export default connect(mapStateToProps, { getNamus, getCrimes })(Results);
+export default connect(mapStateToProps, { getNamus, getCrimes, getUnidentified })(Results);
--- client/src/components/results/UnidentifiedPersonItem.js
+++ client/src/components/results/UnidentifiedPersonItem.js
@@ -4,37 +4,14 @@ import { Link } from 'react-router-dom';
 const UnidentifiedPersonItem = () => {
     return (
         <Fragment>
-            <div className='row'>
-				<div className='col-md-12'>
-					<Link to='/search' className='btn btn-secondary back-btn'>
-						<i id='toggleIcon' className='fa fa-angle-double-down'></i> Back to
-						Search
-					</Link>
-				</div>
-			</div>
-            <h1 className='page-header'>Results</h1>
-            <table className="table table-dark table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Case Number</th>
-                        <th scope="col">Date Found</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Address</th>
-                        <th scope="col">Location Details</th>
-                        <th scope="col">Images</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row"></th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
         </Fragment>
     )
 }
