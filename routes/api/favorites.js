d5225003549b8e1314b57d6094d9821a584917a6
Bishop Bader <bflatbader@users.noreply.github.com>
2020-01-25 21:06:10 -0500

    updated favorites route to append users if case has already been favorited

--- routes/api/favorites.js
+++ routes/api/favorites.js
@@ -1,6 +1,6 @@
 //how we search favorites
 //collection of save cases and array of who saved it
-//put person's id in an array and put the case 
+//put person's id in an array and put the case
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -12,44 +12,52 @@ const Profile = require('../../models/Profile');
 //@desc     Create a favorite
 //@access   Public
 router.post('/:id', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        console.log(user)
-        const newFav = new favCase({
-            Case_Number: req.params.id,
-            Date_Of_Incident: req.body.Date_Of_Incident,
-            Description: req.body.Description,
-            Link: req.body.Link,
-            Users: [{
-                name: user.name,
-                user: user.id
-            }]
-        });
+	try {
+		const user = await User.findById(req.user.id).select(
+			'-password -email -avatar -date'
+		);
+		const favorited = await favCase.find({ Case_Number: req.params.id });
+		// Check if case has already been favorited
+        if (favorited.length > 0) {
+			favorited[0].Users.push(user);
 
-        const fav = await newFav.save();
-        res.json(fav);
+			await favorited[0].save();
+			res.json(favorited);
+		} else {
+			const newFav = new favCase({
+				Case_Number: req.params.id,
+				Date_Of_Incident: req.body.Date_Of_Incident,
+				Description: req.body.Description,
+				Link: req.body.Link,
+				Users: [
+					{
+						name: user.name,
+						_id: user.id
+					}
+				]
+			});
 
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
+			const fav = await newFav.save();
+			res.json(fav);
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
 });
-
 
 //@route   GET api/favorites
 //@desc    GET ALL FAVS
 router.get('/', async (req, res) => {
-    try {
-        const favs = await favPosts.find().sort({
-            Date_Added: -1
-        });
-        res.json(favs);
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server error');
-    }
+	try {
+		const favs = await favPosts.find().sort({
+			Date_Added: -1
+		});
+		res.json(favs);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server error');
+	}
 });
-
-
 
 module.exports = router;
